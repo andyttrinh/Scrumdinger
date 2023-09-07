8087fb9d58a337abb2c2e5c4cc5174710cd8c604
Andy Trinh <andy.thienantrinh@gmail.com>
2023-09-06 20:15:57 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Andy Trinh on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
