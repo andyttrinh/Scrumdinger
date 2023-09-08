fcd4e83771c04cea45a956561becaca63f84883d
Andy Trinh <andy.thienantrinh@gmail.com>
2023-09-07 22:11:16 -0400

    Finished CardView Design

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,14 +11,22 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack {
-            Text(scrum.title).font(.headline)
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes meeting")
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
         
         
     }
---
+++ Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Andy Trinh on 9/7/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self {Self()}
+}
